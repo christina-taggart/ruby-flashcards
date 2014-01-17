a24eb120a4d8754d7ed04699522214ae775a5d7c
Elliot Chalfant & Matthew Higgins <pair+epchalfant.mh120888@devbootcamp.com>
2014-01-17 15:31:10 -0800

    Merged code together

--- source/flashcards.rb
+++ source/flashcards.rb
@@ -1,3 +1,9 @@
+module CardFactory
+  def self.create_card(flashcard_data) # ["Software company in Seattle", "Microsoft"]
+    Card.new(flashcard_data[0], flashcard_data[1])
+  end
+end
+
 class Card
   attr_reader :definition , :concept
   attr_accessor :correct
@@ -11,29 +17,90 @@ class Card
   end
 end
 
-
-test_card = Card.new("Software company in Seattle", "Microsoft")
-test2 = Card.new("Two plus two is...", "four")
-test3 = Card.new("City in which DBC is located? (in CA)", "San Francisco")
-
-
-deck = [test_card, test2, test3]
-
-deck.each do |card|
-  p card
-  print "Your concept: "
-  concept = gets.chomp!
-  if concept == card.concept
-    puts "You are correct"
-    card.correct = true
-  else
-    puts "You should review that one"
+class Deck
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards #this is an array of card objects
   end
 end
 
-correct_cards = deck.select {|card| card.correct}
-incorrect_cards = deck.select{|card| !card.correct}
-puts "You know these really well: "
-correct_cards.each {|card| puts "\t#{card}"}
-puts "You should review these:"
-incorrect_cards.each {|card| puts "\t#{card}"}
+# factory_test = CardFactory.create_card(["Software company in Seattle", "Microsoft"])
+# p factory_test
+
+class Quiz
+  def initialize(deck)
+    @deck = deck
+  end
+  def quiz!
+    @deck.cards.each do |card|
+      p card
+      print "Concept: "
+      answer = gets.chomp!
+      check_answer(answer, card)
+    end
+    correct_or_not
+    display_results
+  end
+  def check_answer(answer, card)
+    if answer == card.concept
+      puts "You are correct"
+      card.correct = true
+    else
+      puts "You should review that one"
+    end
+  end
+  def correct_or_not
+    @correct_cards = @deck.cards.select {|card| card.correct}
+    @incorrect_cards = @deck.cards.select{|card| !card.correct}
+  end
+  def display_results
+    puts "You know these really well: "
+    @correct_cards.each {|card| puts "\t#{card}"}
+    puts "You should review these:"
+    @incorrect_cards.each {|card| puts "\t#{card}"}
+  end
+end
+
+class Controller
+
+  def initialize
+    #make cards?
+    @card_content_array = []
+  end
+
+  def format_txt_file
+    File.readlines("flashcard_samples.txt").each_slice(3){|s|
+      p @card_content_array = [s[0].strip, s[1].strip]
+      }
+  end
+
+end
+
+
+test1 = Card.new("Software company in Seattle", "Microsoft")
+test2 = Card.new("Two plus two is...", "four")
+test3 = Card.new("City in which DBC is located? (in CA)", "San Francisco")
+
+deck = Deck.new([test1, test2, test3])
+
+quiz = Quiz.new(deck)
+quiz.quiz!
+
+# deck.each do |card|
+#   p card
+#   print "Concept: "
+#   concept = gets.chomp!
+#   if concept == card.concept
+#     puts "You are correct"
+#     card.correct = true
+#   else
+#     puts "You should review that one"
+#   end
+# end
+
+# correct_cards = deck.select {|card| card.correct}
+# incorrect_cards = deck.select{|card| !card.correct}
+# puts "You know these really well: "
+# correct_cards.each {|card| puts "\t#{card}"}
+# puts "You should review these:"
+# incorrect_cards.each {|card| puts "\t#{card}"}
