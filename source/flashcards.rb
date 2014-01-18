af87c292db81d748a7fd2d2675df4c74f7142d7c
Apprentice <apprentice@dbc07.local>
2014-01-17 17:38:42 -0800

    release 2: converted to MVC model

--- source/flashcards.rb
+++ source/flashcards.rb
@@ -1,36 +1,14 @@
-# Main file for
-
 require 'csv'
 
-class Deck
-
+class Game
   def initialize
-    @deck = []
     @score = 0
-    add_cards_to_deck
-    ARGV.clear
+    @deck = Deck.new("flashcard_samples.csv").all_cards
     play_game
-  end
-
-  def add_cards_to_deck
-    abort("No file to load") if !File.exists?(ARGV[0])
-
-    CSV.foreach(ARGV[0]) do |csv| #foreach gets every row
-      @deck << Card.new(csv[0], csv[1], csv[2])
-    end
-
-    # @deck #delete me
-
-    #------How to do it with File--------
-    # questions =File.open("flashcard_samples.csv", "r")
-    # questions.each {|line| card_to_push = line.chomp.split(",")
-    #   @deck << Card.new(card_to_push[0], card_to_push[1], card_to_push[2])
-    # }
   end
 
   def play_game
     counter = 1
-    p counter
     until @deck.length == counter
       @deck = @deck.shuffle
       @deck.each do |flashcard|
@@ -42,26 +20,22 @@ class Deck
 
   def ask_question(flashcard)
     number_of_attempts = 3
-
-    puts ""
-    puts "Here is your question. Type \"exit\" to stop playing at anytime or \"score\" to view your current score ."
-    puts "---------"
-    puts flashcard.question
+    new_award = AwardPoints.new(flashcard)
+    new_award.ask_question
     user_answer = gets.chomp
 
     while number_of_attempts > 0
       if user_answer.downcase == flashcard.answer.downcase
-        award_points(flashcard)
+        new_award.correct
         number_of_attempts = 0
       elsif user_answer == "exit"
-        abort("Thanks for playing")
+        new_award.exit
       elsif user_answer == "score"
-        puts "You currently have #{@score} points. Great Job!"
-        puts flashcard.question
+        new_award.score
         user_answer = gets.chomp
       elsif user_answer.downcase != flashcard.answer.downcase
         number_of_attempts -= 1
-        puts "You got the answer wrong. You have #{number_of_attempts} more attempts." if number_of_attempts > 0
+        puts "You got the answer wrong. You have #{number_of_attempts} more attempts."
         if number_of_attempts > 0
           puts flashcard.question
           user_answer = gets.chomp
@@ -71,16 +45,46 @@ class Deck
       end
     end
   end
+end
 
-  def award_points(flashcard)
+class Deck
+  attr_reader :all_cards
+  def initialize(file)
+    @file = file
+    @all_cards = []
+    create_all_cards_array
+  end
+
+  def create_all_cards_array
+    CSV.foreach(@file) do |csv| #foreach gets every row
+      @all_cards << Card.new(csv[0], csv[1], csv[2])
+    end
+  end
+end
+
+class AwardPoints
+  def initialize(flashcard)
+    @flashcard = flashcard
+    @score = 0
+    @number_of_attempts = 3
+  end
+
+  def ask_question
+    puts ""
+    puts "Here is your question. Type \"exit\" to stop playing at anytime or \"score\" to view your current score ."
+    puts "---------"
+    puts @flashcard.question
+  end
+
+  def correct
     case
-      when flashcard.difficulty == "easy"
+      when @flashcard.difficulty == "easy"
         points_for_turn = 1
         @score += points_for_turn
-      when flashcard.difficulty == "medium"
+      when @flashcard.difficulty == "medium"
         points_for_turn = 3
         @score += points_for_turn
-      when flashcard.difficulty == "hard"
+      when @flashcard.difficulty == "hard"
         points_for_turn = 5
         @score += points_for_turn
       else
@@ -89,47 +93,23 @@ class Deck
     puts "Congrats, you got it right and earned #{points_for_turn} points. Your total score is #{@score}"
   end
 
-  def create_flashcard
-    puts "Please enter a question"
-    question_to_add = gets.chomp
-    puts "What is this question's answer?"
-    answer_to_add = gets.chomp
-    puts "Is this question, easy, medium or hard in terms of difficulty"
-    difficulty_to_add = gets.chomp
-
-    CSV.open("flashcard_samples.csv", "ab") do |csv| #add each line to end of csv - no foreach method
-      csv << [question_to_add, answer_to_add, difficulty_to_add]
-    end
-
-    #------How to do it with File--------
-    # test = File.open("flashcard_samples.csv", "a+")
-    # test.puts "#{question_to_add}, #{answer_to_add}, #{difficulty_to_add}"
-
+  def exit
+    abort("Thanks for playing.")
   end
 
-
+  def score
+    puts "You currently have #{@score} points. Great Job!"
+    puts @flashcard.question
+  end
 end
 
-
-
-
-
 class Card
-
   attr_reader :question, :answer, :difficulty
-
   def initialize(question, answer, difficulty)
     @question = question
     @answer = answer
     @difficulty = difficulty
   end
-
 end
 
-
-our_deck = Deck.new
-# our_deck.create_flashcard
-
-
-
-
+Game.new
